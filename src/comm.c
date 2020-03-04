42f32047ba1b9b6e2b7d888bdb0867a90a5e7ae4
Arthmoor <arthmoor@gmail.com>
2020-03-04 16:43:01 -0700

    Pre-auths who changed their names weren't being set to the correct auth_state

--- src/comm.c
+++ src/comm.c
@@ -3190,11 +3190,9 @@ void do_name( CHAR_DATA * ch, const char *argument )
    STRFREE( ch->name );
    ch->name = STRALLOC( arg );
    send_to_char( "Your name has been changed.  Please apply again.\r\n", ch );
-   ch->pcdata->auth_state = 0;
+   ch->pcdata->auth_state = 1;
    return;
 }
-
-
 
 char *hit_prompt( CHAR_DATA * ch )
 {
